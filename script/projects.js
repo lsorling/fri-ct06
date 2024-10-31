14356520186548293e306da7834288da92d2859b
lsorling <lsorling@yahoo.com.sg>
2024-10-31 05:01:15 +0000

    before lesson 17

--- script/projects.js
+++ script/projects.js
@@ -1,1 +1,10 @@
 // 
+const canvas = document.getElementById("canvasBoard");
+console.log(canvas);
+let ctx = canvas.getContext("2d");
+
+// begin drawing a ball
+ctx.beginPath();
+ctx.arc(120, 120, 10, 0, 2 * Math.PI);
+ctx.fill();
+ctx.closePath();
